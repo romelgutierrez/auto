97cc9e421a77a5382c688b7c0dd8337979bd5f96
unknown <romelgutierrez90@gmail.com>
2020-10-27 21:27:09 -0500

    este el se clase modificado internamente

--- src/clase.java
+++ src/clase.java
@@ -1,7 +1,7 @@
 
 public class clase {
     public static void main(String[] args) {
-        System.out.println("hola o");
+        System.out.println("hola este es clase modificado ");
     }
 }
 
