47c1e1914896d5fdc36cc1f9f6759b967812136d
unknown <romelgutierrez90@gmail.com>
2020-10-29 23:12:19 -0500

    agregrado ms practica-v4

--- src/frdo.java
+++ src/frdo.java
@@ -2,5 +2,8 @@
 public class frdo {
     public static void main(String[] args) {
         System.out.println("hola este es clase modificado ");
+
+        System.out.println(" un ms para probar versi dle tag practica-4 ");
+        
     }
 }
